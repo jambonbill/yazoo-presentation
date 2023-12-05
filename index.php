c1b9a623289c7e71559dec972070da5b387e5cae
jambonbill <jambonbill@gmail.com>
2023-12-05 13:00:36 +0100

    up

--- index.php
+++ index.php
@@ -8,12 +8,15 @@
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="theme-color" content="#000000">
+		<link rel="manifest" href="manifest.json">
 		<link rel="stylesheet" href="dist/reset.css">
 		<link rel="stylesheet" href="dist/reveal.css">
 		<link rel="stylesheet" href="dist/theme/black.css" id="theme">
 		<!-- Theme used for syntax highlighting of code -->
 		<link rel="stylesheet" href="plugin/highlight/monokai.css">
 	</head>
+	
 	<body>
 		<div class="reveal">
 			<!-- Any section element inside of this container is displayed as a slide -->
